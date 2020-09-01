b045427a84dd7a4ffb46d7f63ae19006495bf3ab
y.ajisaka <ajisaka@tam-tam.co.jp>
2020-09-01 19:53:46 +0900

    Add: DynamoDBとの接続処理

--- src/handlers/line-bot.js
+++ src/handlers/line-bot.js
@@ -2,30 +2,72 @@
 
 const line = require('@line/bot-sdk');
 const crypto = require('crypto');
+const AWS = require('aws-sdk');
 
-const client = new line.Client({ channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN });
+// LINE Bot-SDKクライアント
+const client = new line.Client(
+  { channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN });
+// AWS SDKクライアント
+AWS.config.update({ region: 'ap-northeast-1' });
+const docClient = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' });
+const tableName = 'line-user-test';
 
 exports.lineBotHandler = async (event) => {
-  for (const { messageId, body, messageAttributes } of event.Records) {
-    let signature = crypto.createHmac('sha256', process.env.CHANNEL_SECRET).update(body).digest('base64');
+  for (const { body, messageAttributes } of event.Records) {
+    let signature = crypto.createHmac('sha256', process.env.CHANNEL_SECRET).
+      update(body).
+      digest('base64');
     let checkHeader = messageAttributes['line-signature']['stringValue'];
     let webhookBody = JSON.parse(body);
 
     if (signature === checkHeader) {
-      if (webhookBody.events[0].replyToken === '00000000000000000000000000000000') {
-        // 接続確認時
-        return { statusCode: 200, body: 'OK' };
-      } else {
-        const message = {
-          type: 'text',
-          text: webhookBody.events[0].message.text
-        };
+      for (const { replyToken, source, message } of webhookBody.events) {
+        // Webhookイベントの処理
+        if (replyToken === '00000000000000000000000000000000') {
+          // 接続確認時
+          return { statusCode: 200, body: 'OK' };
+        } else {
+          // ユーザーデータ取得
+          const searchParams = {
+            TableName: tableName,
+            Key: { 'userId': source.userId }
+          };
 
-        await client.replyMessage(webhookBody.events[0].replyToken, message).
-          then((response) => {
-            return { statusCode: 200, body: message };
-          }).
-          catch((err) => console.error(err));
+          let searchResult;
+          try {
+            searchResult = await docClient.get(searchParams).promise();
+          } catch (e) {
+            console.error('データ取得エラー', e);
+          }
+
+          if (!Object.keys(searchResult).length) {
+            // ユーザーデータがない場合は作成
+            const putParams = {
+              TableName: tableName,
+              Item: {
+                'userId': source.userId,
+                'currentStep': 1
+              }
+            };
+
+            try {
+              await docClient.put(putParams).promise();
+            } catch (e) {
+              console.error('データ挿入エラー', e);
+            }
+          }
+
+          const replyMessage = {
+            type: 'text',
+            text: message.text
+          };
+
+          await client.replyMessage(replyToken, replyMessage).
+            then((response) => {
+              return { statusCode: 200, body: replyMessage };
+            }).
+            catch((err) => console.error(err));
+        }
       }
     } else {
       console.error('署名検証エラー');
