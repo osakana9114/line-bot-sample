0c74fd9a21023711983a82a61938a86466e1652c
y.ajisaka <ajisaka@tam-tam.co.jp>
2020-09-01 13:40:31 +0900

    Update: オウム返しbot

--- src/handlers/line-bot.js
+++ src/handlers/line-bot.js
@@ -1,49 +1,35 @@
 'use strict';
 
-const line = require( '@line/bot-sdk' );
-const crypto = require( 'crypto' );
+const line = require('@line/bot-sdk');
+const crypto = require('crypto');
 
-const client = new line.Client( {channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN} );
+const client = new line.Client({ channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN });
 
-exports.lineBotHandler = function ( event ) {
-    let signature = crypto.createHmac('sha256', process.env.CHANNEL_SECRET).update(event.body).digest('base64');
-    let checkHeader = (event.headers || {})['X-Line-Signature'];
-    let body = JSON.parse(event.body);
+exports.lineBotHandler = async (event) => {
+  for (const { messageId, body, messageAttributes } of event.Records) {
+    let signature = crypto.createHmac('sha256', process.env.CHANNEL_SECRET).update(body).digest('base64');
+    let checkHeader = messageAttributes['line-signature']['stringValue'];
+    let webhookBody = JSON.parse(body);
 
-    if ( signature === checkHeader ) {
-        if ( body.events[0].replyToken === '00000000000000000000000000000000' ) {
-            // 接続確認時
-            return {
-                statusCode: 200,
-                headers: {"X-Line-Status": "OK"}
-            }
-        } else {
-            const message = {
-                "type": "text",
-                "text": "$ LINE emoji $",
-                "emojis": [
-                    {
-                        "index": 0,
-                        "productId": "5ac1bfd5040ab15980c9b435",
-                        "emojiId": "001"
-                    },
-                    {
-                        "index": 13,
-                        "productId": "5ac1bfd5040ab15980c9b435",
-                        "emojiId": "002"
-                    }
-                ]
-            }
+    if (signature === checkHeader) {
+      if (webhookBody.events[0].replyToken === '00000000000000000000000000000000') {
+        // 接続確認時
+        return { statusCode: 200, body: 'OK' };
+      } else {
+        const message = {
+          type: 'text',
+          text: webhookBody.events[0].message.text
+        };
 
-            client.replyMessage( body.events[0].replyToken, message )
-                  .then( ( response ) => {
-                      return {
-                          statusCode: 200,
-                          headers: {"X-Line-Status": "OK"}
-                      }
-                  } ).catch( ( err ) => console.error( err ) );
-        }
+        await client.replyMessage(webhookBody.events[0].replyToken, message).
+          then((response) => {
+            return { statusCode: 200, body: message };
+          }).
+          catch((err) => console.error(err));
+      }
     } else {
-        console.error( '署名認証エラー' );
+      console.error('署名検証エラー');
+      return { statusCode: 401, body: 'Unauthorized' };
     }
-}
+  }
+};
